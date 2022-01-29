5cc76a394a96d203fab83e4db1026acca5cabac5
mmankey <mark.mankey@gmail.com>
2022-01-29 17:54:44 -0600

    lesson 1

---
+++ Lesson_001/Lesson_001.ino
@@ -0,0 +1,15 @@
+/*
+  https://www.youtube.com/watch?v=fJWR7dBuc18
+*/
+
+void setup() {
+  pinMode(13,OUTPUT);
+    
+}
+
+void loop() {
+    digitalWrite(13,HIGH);
+    delay(1000);
+    digitalWrite(13,LOW);
+    delay(1000);
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ArduinoPlayGroud
 Messing with and Arduino
+
+## Arduino and VS Code
+https://maker.pro/arduino/tutorial/how-to-use-visual-studio-code-for-arduino
